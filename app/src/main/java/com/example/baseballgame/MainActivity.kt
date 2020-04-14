f95ab182f8ed0c48089d59d46a8343947fa7b646
eunjung04 <peunj04@gmail.com>
2020-04-14 21:51:40 +0900

    어댑터 리스트뷰 연결

--- app/src/main/java/com/example/baseballgame/MainActivity.kt
+++ app/src/main/java/com/example/baseballgame/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.baseballgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.baseballgame.adapters.ChatAdapters
 
 class MainActivity : BaseActivity() {
+
+    val chatings=ArrayList<Chat>()
+    var
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        override fun setValues(){
+
+            mChatAdapter=ChatAdapter
+        }
     }
 }
--- app/src/main/java/com/example/baseballgame/adapters/ChatAdapter.kt
+++ app/src/main/java/com/example/baseballgame/adapters/ChatAdapter.kt
@@ -1,6 +1,65 @@
 package com.example.baseballgame.adapters
 
 import android.content.Context
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import com.example.baseballgame.BaseActivity
 
-class ChatAdapters(context: Context, resId:Int, list : ArrayList<Chat>)
+class MainActivity : BaseActivity() {
+
+
+    override fun setupEvents() {
+
+
+        //리니어 레이아웃의 클릭 이벤트
+
+        singUpBtn.setOnClickListener{
+
+            Toast.makeText(mContext, "회원가입 시도합니다.", Toast.LENGTH_SHORT).show()
+        }
+
+
+
+        idEdt.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val inputVal=s.toString()
+                if(inputVal.length == 0){
+                    idStatusTxt.text="최소 여섯글자 이상으로 해주세요."
+                }
+                else if(inputVal.length<6){
+                    idStatusTxt.text="아이디가 너무 짧습니다."
+                }
+
+                else{
+                    idStatusTxt.text="사용해도 좋은 아이디입니다."
+                }
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+
+        })
+
+    }
+
+    override fun setValues() {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
 }
